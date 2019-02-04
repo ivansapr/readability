fe9a1de05c0272397a8d473dde237c176295ad6c
Иван <ivan.saprykin@live.com>
2019-02-04 13:43:38 +0500

    import

--- parse/__init__.py
+++
@@ -1,8 +0,0 @@
-import json
-import os
-import re
-import textwrap
-from urllib.parse import urlparse
-
-import requests
-from bs4 import BeautifulSoup, element
--- parse/parse.py
+++ parse/parse.py
@@ -1,3 +1,13 @@
+import json
+import os
+import re
+import textwrap
+from urllib.parse import urlparse
+
+import requests
+from bs4 import BeautifulSoup, element
+
+
 class Parse:
 
     CONFIG_PATH = '{}{}'.format(
